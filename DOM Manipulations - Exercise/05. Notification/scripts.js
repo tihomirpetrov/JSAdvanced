c99e843220063e71bae00e614bdbf062f5f9d87e
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-06-18 20:42:42 +0300

    initial commit

--- DOM Manipulations - Exercise/05. Notification/scripts.js
+++ DOM Manipulations - Exercise/05. Notification/scripts.js
@@ -1,3 +1,9 @@
 function notify(message) {
-    console.log('TODO:...');
+    let notificationDivElement = document.getElementById('notification');
+    notificationDivElement.textContent = message;
+    notificationDivElement.style.display = 'block';
+
+    setTimeout(function () {
+        notificationDivElement.style.display = 'none';
+    }, 2000);
 }
